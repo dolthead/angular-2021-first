c268222dd5c4dbc4c7166673a9774701bca8845a
Todd H <github@simplybest.com>
2023-09-22 08:15:06 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# first
+first ionic project spring 2021
+```
+npm i
+ionic serve
+
+ionic cap add ios
+ionic cap sync
+ionic cap open ios
+```
+Push the > Run button to build and run the app in an emulator.
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.simplybest.mtech.first',
+  appName: 'ASL',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
